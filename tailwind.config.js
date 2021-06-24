bce131f0d6ffd02349dcdf8c9b421bc34d16181e
soufiane <soufiane.stdin@gmail.com>
2021-06-24 17:28:12 +0100

    first init tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container : {
+      center : true , 
+      padding : '2rem' , 
+    } ,
+    extend: {
+      colors: {
+       soufiane : '#000' ,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
